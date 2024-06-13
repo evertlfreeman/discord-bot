fec36cfa1a9bf00b5956797095171cde516be62a
evertlfreeman <evertlfreeman@gmail.com>
2024-06-13 15:55:41 -0700

    first-push

---
+++ discordbot/discordbot.py
@@ -0,0 +1,44 @@
+import discord
+import os
+import random
+from ec2_metadata import ec2_metadata
+from dotenv import load_dotenv
+
+load_dotenv('token.env')
+
+token = str(os.getenv('TOKEN'))
+
+intents = discord.Intents.default()
+intents.messages = True
+
+
+
+client = discord.Client(intents=intents)
+
+#print('This is my Ec2_metadata.region:', ec2_metadata.region)
+#print('This is my Ec2_metadata.instance.id:', ec2_metadata.instance_id)
+
+@client.event
+async def on_ready():
+    print("Logged in as a bot {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    username = str(message.author).split("#")[0]
+    channel = str(message.channel.name)
+    user_message = str(message.content)
+
+    print(f'Message {user_message} by {username} on {channel}')
+    if message.author == client.user:
+        return
+    if channel == "random":
+           if user_message.lower() == "hello" or user_message.lower() == "hi":
+            await message.channel.send(f'Hello {username}')
+            return
+           elif user_message.lower() == "bye":
+            await message.channel.send(f'Bye {username}')
+           elif user_message.lower() == "tell me a joke":
+                jokes = ["your life.", "Did you hear the rumor about butter? Well, I'm not going to spread it."]
+                await message.channel.send(random.choice(jokes))
+
+client.run(token)
